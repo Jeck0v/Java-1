1b5cc1d36a746c1d0f8a983eac7cc7ee56b16d38
Arnaud Fischer <149910271+Jeck0v@users.noreply.github.com>
2024-06-04 00:37:36 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -14,11 +14,32 @@ public class Main {
         int age = 18;
         System.out.println("I am " + age + " years old");
 
+       
+        char copyrightSymbol =  '\u00A9';
+        System.out.println("This is the copyright symbol " + copyrightSymbol);
+        
+        char number1 = 12;
+        int number2 = 6;
+
+        // addition
+        System.out.println(number1 + number2);
+        // soustraction
+        System.out.println(number1 - number2);
+        // multiplication
+        System.out.println(number1 * number2);
+        // division
+        System.out.println(number1 / number2);
+        // modulo
+        System.out.println(number1 % number2);
+
+        int score=10;
+        score--;
+        score++;
+        System.out.println(score);
+        
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
-        char copyrightSymbol =  '\u00A9';
-        System.out.println("This is the copyright symbol " + copyrightSymbol);
     }
 }
 

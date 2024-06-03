88fc21ba5820479fad268643cd8572d93d366b58
Arnaud Fischer <149910271+Jeck0v@users.noreply.github.com>
2024-06-03 23:28:19 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        // Types:
+        // int
+        // byte
+        // short
+        // long
+        // double
+        // float
+        // boolean
+        // char
+
+        int age = 18;
+        System.out.println("I am " + age + " years old");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+        char copyrightSymbol =  '\u00A9';
+        System.out.println("This is the copyright symbol " + copyrightSymbol);
+    }
+}
+
+//commentaire
